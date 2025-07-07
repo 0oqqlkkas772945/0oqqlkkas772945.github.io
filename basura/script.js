ff729f1397e4d211e4d0a855c8ab2907e77c7a99
0oqqlkkas772945 <0oqqlkkas772945@gmail.com>
2025-07-07 21:19:50 +0000

    me la pela

--- basura/script.js
+++ basura/script.js
@@ -1,6 +1,6 @@
 const terminal = document.getElementById("terminal");
 
-const fullText = `
+const lines = `
 ORGANIZACIÓN OMEGA - DIVISIÓN ECLIPSE
 CLASIFICACIÓN: XK-9 - ULTRA SECRETO
 
@@ -26,34 +26,21 @@ Observaciones
 Las grabaciones de los drones revelan siluetas humanas siendo consumidas por haces de luz emitidos desde el obelisco. Los análisis iniciales sugieren una inteligencia alienígena manipulando las anomalías. Se ha autorizado el uso de Protocolo Épsilon-3, incluyendo experimentos con sujetos voluntarios de bajo perfil.
 
 Recomendaciones
-Recomendando el despliegue inmediato de la Unidad Psi-7 para neutralizar la amenaza. Sugerimos también la eliminación de testigos y la supresión de datos en la red pública. Todo rastro de esta operación debe ser borrado para preservar la integridad de la Organización O`;
+Recomendando el despliegue inmediato de la Unidad Psi-7 para neutralizar la amenaza. Sugerimos también la eliminación de testigos y la supresión de datos en la red pública. Todo rastro de esta operación debe ser borrado para preservar la integridad de la Organización O`.split("\n");
 
-const typingSound = new Audio("typewriter.mp3");
-typingSound.volume = 0.1;
-
-const interferenceSound = new Audio("interference.mp3");
-interferenceSound.volume = 0.4;
-
-let index = 0;
-const typingSpeed = 20;
-
-function typeChar() {
-  if (index < fullText.length) {
-    terminal.textContent += fullText[index];
-    typingSound.currentTime = 0;
-    typingSound.play();
-    if (fullText.substring(index - 30, index).includes("Todo rastro de esta operación")) {
+let i = 0;
+function typeLine() {
+  if (i < lines.length) {
+    terminal.innerHTML += lines[i] + "\n";
+    i++;
+    if (lines[i - 1].includes("Todo rastro de esta operación")) {
       setTimeout(() => {
-        interferenceSound.play();
-        setTimeout(() => {
-          window.location.href = "redacted.html";
-        }, 2000);
-      }, 500);
-      return;
+        window.location.href = "redacted.html";
+      }, 2000);
+    } else {
+      setTimeout(typeLine, 100);
     }
-    index++;
-    setTimeout(typeChar, typingSpeed);
   }
 }
 
-window.onload = typeChar;
+window.onload = typeLine;
