5f8ade72c98936746e1ed607ec3177480ec6a50e
0oqqlkkas772945 <0oqqlkkas772945@gmail.com>
2025-07-08 08:14:02 +0000

    HOMERO SINSON

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 const terminal = document.getElementById("terminal");
 
 const fullText = `
-ORGANIZACIÓN OMEGA - DIVISIÓN ECLIPSE
+[HIPERVÍNCULO BLOQUEADO]
 CLASIFICACIÓN: XK-9 - ULTRA SECRETO
 
-INFORME DE OPERACIÓN: SOMBRA ETERNA
+INFORME DE OPERACIÓN: LA NOCHE MÁS OSCURA
 
 CLASIFICADO XK-9
-ACCESO RESTRINGIDO - AUTORIZACIÓN SOLO PARA AGENTES DE NIVEL ALFA
-DESTINATARIO: DIRECTOR EJECUTIVO, DIVISIÓN ECLIPSE
+ACCESO RESTRINGIDO - AUTORIZACIÓN SOLO PARA MTF DE RANGO ALFA
+DESTINATARIO: DIVISIÓN [REDACTED]
 FECHA: 07-07-2025
 ESTADO: EN CURSO
 
-Introducción
-Registrando hallazgos preliminares de la Operación Sombra Eterna, una misión encubierta diseñada para investigar anomalías cuánticas detectadas en el Sector Nebuloso-7. Este informe detalla encuentros con entidades no identificadas y los métodos empleados para contener las brechas espacio-temporales. Advertencia: los datos aquí contenidos podrían comprometer la estabilidad psíquica del personal no autorizado.
+Descripción de la situación
+Registrando hallazgos preliminares de la Operación Sombra Eterna, una misión encubierta diseñada para experimentar con anomalías detectadas en [REDACTED]. Este informe detalla encuentros con entidades no identificadas y los métodos empleados para su contención.
+Advertencia: los datos aquí contenidos podrían comprometer la estabilidad psíquica del personal no autorizado.
 
 Detalles de la Operación
-Comenzando con la infiltración en las ruinas de la Colonia X-14, nuestro equipo de agentes de élite, designados como Unidad Theta-9, reportó la presencia de un obelisco negro emitiendo pulsos de energía desconocida. Los sujetos de prueba expuestos a dichos pulsos mostraron síntomas de desintegración molecular y alucinaciones catastróficas.
+Comenzó con la infiltración en las ruinas del laboratorio subterráneo. La Unidad Theta-9 reportó la presencia de un obelisco negro ominoso emergente de un sumidero, cuya profundidad no se pudo determinar, emitiendo pulsos de energía desconocida. Los sujetos de prueba expuestos a dichos pulsos mostraron síntomas de [REDACTED].
 
-- Ubicación: Coordenadas cifradas - Sector Nebuloso-7, Cuadrante 472.
-- Recursos Desplegados: 12 agentes, 3 drones de reconocimiento, 1 dispositivo de contención de fase.
-- Pérdidas: 4 agentes neutralizados por exposición directa; estado de los cuerpos: irreconocible.
+- Ubicación: 41.595830, 1.825195.
+- Recursos Desplegados: 16 agentes, 3 drones de reconocimiento, 1 dispositivo de contención de fase.
+- Pérdidas: [REDACTED]; estado de los cuerpos: irreconocible.
 
 Observaciones
-Las grabaciones de los drones revelan siluetas humanas siendo consumidas por haces de luz emitidos desde el obelisco. Los análisis iniciales sugieren una inteligencia alienígena manipulando las anomalías. Se ha autorizado el uso de Protocolo Épsilon-3, incluyendo experimentos con sujetos voluntarios de bajo perfil.
+Las grabaciones de los drones revelan [REDACTED] desde el obelisco. Los análisis iniciales sugieren que dichas anomalías no ocurren aleatoriamente. Se ha autorizado el uso de Protocolo Épsilon-3, incluyendo experimentos con sujetos voluntarios de bajo perfil.
 
 Recomendaciones
 Recomendando el despliegue inmediato de la Unidad Psi-7 para neutralizar la amenaza. Sugerimos también la eliminación de testigos y la supresión de datos en la red pública. Todo rastro de esta operación debe ser borrado para preservar la integridad de la Organización O`;
 
 const typingSound = new Audio("typewriter.mp3");
-typingSound.volume = 0.2;
+typingSound.volume = 0.3;
 
 const interferenceSound = new Audio("interference.mp3");
-interferenceSound.volume = 1.0;
+interferenceSound.volume = 10.0;
 
 let index = 0;
-const typingSpeed = 10;
+const typingSpeed = 8;
 
 function typeChar() {
   if (index < fullText.length) {
